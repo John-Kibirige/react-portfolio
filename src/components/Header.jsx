63006e6373b210cc3b87f5cfc76fb47ef2eb5ca2
John Kibirige <kibirigejohn005@gmail.com>
2022-10-10 13:51:08 +0300

    Scale header to fit well on desktop

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { HiOutlineMail, FiChevronRight } from 'react-icons/all';
 
 const Header = () => {
@@ -6,6 +6,19 @@ const Header = () => {
   const toggleMenu = () => {
     setMenu((prev) => !prev);
   };
+
+  const [windowSize, setWindowSize] = useState(window.innerWidth);
+  useEffect(() => {
+    window.addEventListener('resize', () => {
+      setWindowSize(window.innerWidth);
+    });
+  });
+
+  useEffect(() => {
+    if (windowSize >= 640) {
+      setMenu(false);
+    }
+  });
 
   return (
     <header className="header flex justify-between items-center border pl-4 pr-6 py-6 relative">
@@ -23,45 +36,45 @@ const Header = () => {
             menuClicked
               ? 'block absolute top-24 border left-0 w-full h-[100vh]'
               : 'hidden'
-          }`}>
-          <ul className="lists flex flex-col px-8">
-            <li className="item ">
+          } sm:block sm:static`}>
+          <ul className="lists flex flex-col px-8 sm:flex-row sm:items-center">
+            <li className="item mr-8">
               {' '}
               <a
                 href="#portfolio"
-                className="portfolio block text-lg font-medium transition-all hover:text-zei-400 flex justify-between items-center">
+                className="portfolio text-lg font-medium transition-all hover:text-zei-400 flex justify-between items-center sm:text-base sm:hover:underline">
                 Portfolio{' '}
-                <FiChevronRight className="text-zei-400 text-xl transition-all cursor-pointer hover:border " />
+                <FiChevronRight className="text-zei-400 text-xl transition-all cursor-pointer hover:border sm:hidden" />
               </a>
             </li>
-            <li className="item my-4 ">
+            <li className="item my-4 mr-8">
               {' '}
               <a
                 href="#about"
-                className="about  text-lg font-medium transition-all hover:text-zei-400 flex justify-between items-center">
+                className="about  text-lg font-medium transition-all hover:text-zei-400 flex justify-between items-center sm:text-base sm:hover:underline">
                 About
-                <FiChevronRight className="text-zei-400 text-xl transition-all cursor-pointer hover:border " />
+                <FiChevronRight className="text-zei-400 text-xl transition-all cursor-pointer hover:border sm:hidden " />
               </a>
             </li>
-            <li className="item mb-4 ">
+            <li className="item mb-4 sm:mb-0 mr-8">
               <a
                 href="#contact"
-                className="contact text-lg font-medium transition-all hover:text-zei-400 flex justify-between items-center">
+                className="contact text-lg font-medium transition-all hover:text-zei-400 flex justify-between items-center sm:text-base sm:hover:underline">
                 Contact
-                <FiChevronRight className="text-zei-400 text-xl transition-all cursor-pointer hover:border " />
+                <FiChevronRight className="text-zei-400 text-xl transition-all cursor-pointer hover:border  sm:hidden" />
               </a>
             </li>
-            <li className="item hidden">
+            <li className="item hidden sm:block">
               {' '}
               <a href="#contact">
-                <HiOutlineMail className="text-zei-400 text-xl" />
+                <HiOutlineMail className="hover:text-zei-400 text-xl" />
               </a>
             </li>
           </ul>
         </div>
 
         <div
-          className="humbugger-menu cursor-pointer border"
+          className="humbugger-menu cursor-pointer sm:hidden"
           onClick={toggleMenu}>
           <span
             className={`bar w-6 h-0.5 block transition-all bg-white rounded ${
