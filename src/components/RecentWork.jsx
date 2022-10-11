0a4be7977b4e30c8f8c18032662a38223c0569a8
John Kibirige <kibirigejohn005@gmail.com>
2022-10-11 11:14:07 +0300

    Render all projects using the data inside the Recent work component

--- src/components/RecentWork.jsx
+++ src/components/RecentWork.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import Project from './Project';
+import data from '../data/data';
 
 const RecentWork = () => {
+  const projectElements = data.map((project) => {
+    const { image, title, description, technologies, id } = project;
+    return (
+      <Project
+        key={id}
+        image={image}
+        title={title}
+        description={description}
+        technologies={technologies}
+      />
+    );
+  });
+
   return (
     <section className="recent-work sm:my-36">
-      <h1 className="text-[40px] uppercase text-zei-400 border-b border-gray-500 mb-5 pb-8 sm:capitalize sm:tracking-wide sm:font-bold  sm:text-[4.26vw] sm:mx-[9.1vw] ">
+      <h1 className="text-[40px] uppercase text-zei-400 border-b border-gray-500 mb-5 pb-7 sm:capitalize sm:tracking-wide sm:font-bold  sm:text-[4.26vw] sm:mx-[9.1vw] ">
         My recent work
       </h1>
+      <div className="projects-container pt-5">{projectElements}</div>
     </section>
   );
 };
